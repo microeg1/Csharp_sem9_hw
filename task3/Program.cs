a096dd1e5d0148f3d7734813d75e74c7add0a75b
Egor <pro100egar@gmail.com>
2022-09-12 13:10:47 +0700

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+static uint A(uint n, uint m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return A(n - 1, 1);
+    else
+      return A(n - 1, A(n, m - 1));
+}
+Console.WriteLine(A(1,4));
