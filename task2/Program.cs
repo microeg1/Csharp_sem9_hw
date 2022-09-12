c7280ab3a05e211e408917e4f69e6875b4e7fd2e
Egor <pro100egar@gmail.com>
2022-09-12 13:10:39 +0700

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+Console.Write("Введи N: ");
+int N = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи M: ");
+int M = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"N={N}, M={M}");
+int sum = 0;
+if (M>N)
+{   
+    for (int i = N; i <= M; i++)
+    {
+        Console.Write($" {i}");
+        sum += i;
+    }
+}
+else
+{
+    for (int i = M; i <= N; i++)
+    {
+        Console.Write($" {i}");
+        sum +=i;
+    }
+}
+Console.WriteLine($"");
+Console.WriteLine($"Сумма чисел равна  {sum}");
